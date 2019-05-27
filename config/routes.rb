03a2e2ce5dd4b000d875a86d2ba3f040eb22c6be
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-05-27 17:16:50 +0000

    add: --> リーダー権限を渡す機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resource :user
   
   resources :teams do
+    put :change
     resources :assigns, only: %w(create destroy)
     resources :agendas, shallow: true do
       resources :articles do
